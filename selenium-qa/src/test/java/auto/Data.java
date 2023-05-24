77aef47b0c6323a537a01d2c6a7c2a4c38f60d11
inalamadhu <madhuinala0109@gmail.com>
2023-05-24 09:35:34 +0530

    updated scripts

---
+++ selenium-qa/src/test/java/auto/Data.java
@@ -0,0 +1,30 @@
+package auto;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Data {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"/selenium-qa/src/resources/data.xlsx");
+        XSSFWorkbook wb = new XSSFWorkbook(fis);
+        Sheet sheet = wb.getSheet("sheet1");
+        int rowCount = sheet.getLastRowNum();
+        DataFormatter form = new DataFormatter();
+        Row row = sheet.getRow(1);
+        for(int i=0; i<row.getLastCellNum(); i++){
+            Cell cell = row.getCell(i);
+            System.out.println(form.formatCellValue(cell));
+        }
+
+    }
+
+}
--- selenium-qa/src/test/java/auto/SmokeTest.java
+++ selenium-qa/src/test/java/auto/SmokeTest.java
@@ -1,20 +1,31 @@
 package auto;
 
 import core.config.BaseTest;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+
 public class SmokeTest extends BaseTest {
 
     @Test(groups={"gridTable"})
-    public void gridTable(){
+    public void gridTable() throws IOException {
         WebDriver driver  = getDriver();
         WebElement elm = driver.findElement(By.cssSelector(".tsc_table_s13"));
         String str = elm.findElement(By.xpath("//span[text()='Taipei']/parent::td/parent::tr/th")).getText();
         // elm.findElement(By.xpath("//span[text()='Taipei']/parent::td/parent::tr/th")).getText()
         System.out.println(str);
+
+
     }
 
 
